0154d2ad961c752a023b88f774d08a7e9e1288f9
Emiliano Barea <emilianobarea8@gmail.com>
2023-07-17 17:05:01 -0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Canvas from "./canvas";
+import Costumizer from "./pages/Costumizer";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Costumizer />
+    </main>
+  );
+}
+
+export default App;
